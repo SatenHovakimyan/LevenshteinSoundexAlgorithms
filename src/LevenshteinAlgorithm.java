fd94739cb6f4a7f7e12bdf29871ed19f85adb76c
SatenHovakimyan <saten.hovakimyan5@gmail.com>
2023-11-29 22:28:33 +0400

    Added printMatrix method, and switch case in main method to select which algorithm to execute.

--- src/LevenshteinAlgorithm.java
+++ src/LevenshteinAlgorithm.java
@@ -1,4 +1,26 @@
 public class LevenshteinAlgorithm {
+
+    private static void printMatrix(int[][] matrix, String firstWord, String secondWord) {
+        System.out.println("Matrix:");
+        System.out.print("      ");
+        for (char c : firstWord.toCharArray()) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+        System.out.print("    ");
+        for (int j = 0; j <= firstWord.length(); j++) {
+            System.out.print(matrix[0][j] + " ");
+        }
+        System.out.println();
+        for (int i = 1; i <= secondWord.length(); i++) {
+            System.out.print(secondWord.charAt(i - 1) + "   " + matrix[i][0] + " ");
+            for (int j = 1; j <= firstWord.length(); j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public static int calculateDistanceOfLevenshtein(String firstWord, String secondWord) {
         int columns = firstWord.length();
         int rows = secondWord.length();
@@ -12,22 +34,12 @@ public class LevenshteinAlgorithm {
         }
 
         for (int i = 1; i <= rows; i++) {
-//            for (int j = 1; j <= columns; j++) {
-//                if(firstWord.charAt(j - 1) == secondWord.charAt(i - 1)) {
-//                    matrixLevenshtein[i][j] = matrixLevenshtein[i - 1][j - 1];
-//                } else {
-//                    matrixLevenshtein[i][j] = Math.min(Math.min(matrixLevenshtein[i][j - 1], matrixLevenshtein[i - 1][j]), matrixLevenshtein[i - 1][j - 1]) + 1;
-//                }
-//            }
-
             for (int j = 1; j <= columns; j++) {
                 int diffCost = (firstWord.charAt(j - 1) == secondWord.charAt(i - 1)) ? 0 : 1;
                 matrixLevenshtein[i][j] = Math.min(Math.min(matrixLevenshtein[i][j - 1] + 1, matrixLevenshtein[i - 1][j] + 1), matrixLevenshtein[i - 1][j - 1] + diffCost);
             }
         }
-
+        printMatrix(matrixLevenshtein, firstWord, secondWord);
         return matrixLevenshtein[rows][columns];
     }
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//            System.out.println("Enter a word for getting its Soundex index(or 'exit' to quit): ");
-//            String word = scanner.nextLine();
-//            if(word.equalsIgnoreCase("exit")) {
-//                break;
-//            }
-//            System.out.println("Soundex index of " + word + "is: " + SoundexAlgorithm.soundex(word));
-//        }
-//        scanner.close();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(LevenshteinAlgorithm.calculateDistanceOfLevenshtein("saturday", "sunday"));
-        System.out.println(LevenshteinAlgorithm.calculateDistanceOfLevenshtein("rainy", "sunny"));
-        System.out.println(LevenshteinAlgorithm.calculateDistanceOfLevenshtein("intention", "execution"));
+        while (true) {
+            System.out.println("Choose an option:");
+            System.out.println("1. Get Soundex index of a word");
+            System.out.println("2. Calculate Levenshtein distance between two words");
+            System.out.println("Type 'exit' to quit.");
+
+            String option = scanner.nextLine();
+
+            if (option.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            switch (option) {
+                case "1":
+                    System.out.println("Enter a word to get its Soundex index: ");
+                    String soundexWord = scanner.nextLine();
+                    System.out.println("Soundex index of " + soundexWord + " is: " + SoundexAlgorithm.soundex(soundexWord));
+                    break;
+
+                case "2":
+                    System.out.println("Enter two words to calculate Levenshtein distance:");
+                    System.out.print("First word: ");
+                    String firstWord = scanner.nextLine().toUpperCase();
+                    System.out.print("Second word: ");
+                    String secondWord = scanner.nextLine().toUpperCase();
+                    int levenshteinDistance = LevenshteinAlgorithm.calculateDistanceOfLevenshtein(firstWord, secondWord);
+                    System.out.println("Levenshtein distance between " + firstWord + " and " + secondWord + " is: " + levenshteinDistance);
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please choose again.");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("Exiting the program.");
+        scanner.close();
     }
 }
